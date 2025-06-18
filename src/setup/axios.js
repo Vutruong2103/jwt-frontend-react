616b60c863a954564fd9734dda2d122acbb92207
quangvu <vuff12345@gmail.com>
2025-06-19 00:15:14 +0700

    check quyen cho all router

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
@@ -29,7 +30,20 @@ instance.interceptors.response.use(function (response) {
 }, function (error) {
   // Any status codes that falls outside the range of 2xx cause this function to trigger
   // Do something with response error
-  return Promise.reject(error);
+  const status = error && error.response && error.response.status || 500;
+  switch (status) {
+    case 401: {
+      toast.error("Unauthorized access - please log in again.");
+      return Promise.reject(error);
+    }
+    case 403: {
+      toast.error('you don\'t have permission to access this resource');
+      return Promise.reject(error);
+    }
+    default: {
+      return Promise.reject(error);
+    }
+  }
 });
 
 export default instance;
